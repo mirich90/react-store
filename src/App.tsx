81aeaf7c1cbcbef5b22eb2bbb472f8bfad3d997a
genkoskarev <gen.koskarev@gmail.com>
2023-11-12 19:10:03 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import NotFound from "./pages/NotFound";
-import FullPizza from "./pages/FullPizza";
 import "./scss/app.scss";
 import MainLayout from "./layouts/MainLayout";
 
+const Cart = React.lazy(
+  () => import(/* webpackChunkName: "Cart" */ "./pages/Cart")
+);
+const FullPizza = React.lazy(
+  () => import(/* webpackChunkName: "FullPizza" */ "./pages/FullPizza")
+);
+const NotFound = React.lazy(
+  () => import(/* webpackChunkName: "NotFound" */ "./pages/NotFound")
+);
 //https://6531a2474d4c2e3f333d3049.mockapi.io/pizzas
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route path="" element={<Home />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="pizzas/:id" element={<FullPizza />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Загрузка...</div>}>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="" element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="pizzas/:id" element={<FullPizza />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
